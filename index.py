ab85311e944b1a1e6c43d55a3376e263077752e7
Ben Forster <forsteremailaddress@gmail.com>
2018-12-30 13:37:30 +0000

    Update index.py

--- index.py
+++ index.py
@@ -4,19 +4,19 @@ from discord.ext import commands
 import asyncio
 import time
 
-Client = discord.Client ()
-client = command.Bot (command_prefix = "!")
+Client = discord.Client()
+client = commands.Bot(command_prefix = "!")
 
 @client.event
 async def on_ready():
-    print("Bot is ready!")
+    print ("Bot is ready!")
 
 @client.event
 async def on_message(message):
     if message.content == "cookie":
         await client.send_message(message.channel, ":cookie:")
 
+
+
         
-
-
-client.run(process.env).token
+client.run(process.env.token)
